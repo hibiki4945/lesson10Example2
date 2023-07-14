d707d94f889377bf8439cc174d776458a29524e3
hibiki4945 <wc4910720@gmail.com>
2023-07-14 15:50:41 +0800

    123

--- main.js
+++ main.js
@@ -1,118 +1,66 @@
-const mealList = [
-  {
-      "name": "食在一方",
-      "category": ["brunch"],
-      "filter": false
-  },
-  {
-      "name": "日十",
-      "category": ["brunch"],
-      "filter": false
-  },
-  {
-      "name": "鹽行站",
-      "category": ["buffet", "rice"],
-      "filter": false
-  },
-  {
-      "name": "鍋燒意麵",
-      "category": ["noodle"],
-      "filter": false
-  },
-  {
-      "name": "炒飯",
-      "category": ["rice"],
-      "filter": false
-  },
-  {
-      "name": "八方雲集",
-      "category": ["others"],
-      "filter": false
-  },
-  {
-      "name": "火鍋",
-      "category": ["others"],
-      "filter": false
-  },
-  {
-      "name": "韓式料理",
-      "category": ["rice", "noodle"],
-      "filter": false
-  },
-  {
-      "name": "小飯糰大飯糰",
-      "category": ["rice"],
-      "filter": false
-  },
-  {
-      "name": "後校門滷肉飯",
-      "category": ["rice", "noodle"],
-      "filter": false
-  },
-  {
-      "name": "魚耶",
-      "category": ["brunch", "rice", "noodle"],
-      "filter": false
-  },
-  {
-      "name": "吳家鴨香飯",
-      "category": ["rice", "noodle"],
-      "filter": false
-  },
-  {
-      "name": "施家雞肉飯",
-      "category": ["rice"],
-      "filter": false
-  },
-  {
-      "name": "黑盤",
-      "category": ["rice", "noodle"],
-      "filter": false
-  },
-  {
-      "name": "神武拉麵",
-      "category": ["noodle"],
-      "filter": false
-  },
-  {
-      "name": "森井丼飯",
-      "category": ["rice"],
-      "filter": false
-  },
-  {
-      "name": "雙醬咖哩",
-      "category": ["rice"],
-      "filter": false
-  },
-  {
-      "name": "麺や青鈴",
-      "category": ["noodle"],
-      "filter": false
-  },
-  {
-      "name": "金拱門",
-      "category": ["noodle", "rice"],
-      "filter": false
-  }
-]
 
-// checkbox
+
+// // checkbox
 const brunchEl = document.getElementById("brunch");
 const riceEl = document.getElementById("rice");
 const noodleEl = document.getElementById("noodle");
 const buffetEl = document.getElementById("buffet");
 const othersEl = document.getElementById("others");
 
-// 組成 DOM List
+// // 組成 DOM List
 const checklist = [brunchEl, riceEl, noodleEl, buffetEl, othersEl];
 
-// button
+// // button
 const btnEl = document.querySelector("#roll");
 
-// order
+// // order
 const orderEl = document.getElementById("order");
 
-// 監聽
+////////////////////非同步//////////////////////
+// 原寫法
+// fetch("mealList.json")
+//     .then(function(response){
+//         //     將json格式資料 轉成 js物件
+//         return response.json;
+//     })
+//     // 確保 .then裡的程式碼 一定是在 資料確定抓回來之後 才執行
+//     .then(function(data){
+//         // 要做的事 都放在 這個.then 的裡面
+//         console.log(data);
+//         // 賦值
+//         mealList = data;
+//     })
+    
+
+let mealList = [];
+// console.log("抓資料前");
+// 簡易寫法
+fetch("mealList.json")
+    // 當.then內 只有一個變數時(可將 '()' 省略)
+    // 當方法(例 res)內 只有 一行程式碼時(可將 '{}' 省略)
+    .then(res => res.json())
+    .then(data => {
+        console.log("fetch 抓完資料了!");
+        // console.log(data);
+        mealList = data;
+        console.log(mealList);
+    })
+// console.log("抓資料後");
+// console.log(mealList);
+
+////////////////////非同步//////////////////////
+
+// // 抓到 空陣列(因 fetch會先進 WebAPIs(因此 此時fetch 未被執行))
+// console.log(mealList);
+
+// btnEl.addEventListener("click", ()=>{
+//     console.log("EventListener Click");
+// })
+
+
+
+//////////////////////////////////////////
+// // 監聽
 btnEl.addEventListener("click", ()=>{
   // console.log(checklist);
   // console.dir(brunchEl);
